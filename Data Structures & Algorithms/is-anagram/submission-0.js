15c4d03c9124c9f8b2c7fa1dcdc80b28d077ea85
Kaloyan Rizov <120266068+KaloyanRizov28@users.noreply.github.com>
2026-09-15 13:32:00 +0300

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        let mapS = new Map();
+        let mapT = new Map();
+
+
+        for (let char of s) {
+            if (mapS.has(char)) {
+                mapS.set(char, mapS.get(char) + 1)
+            }
+            else (
+                mapS.set(char, 1)
+            )
+        }
+        for (let char of t) {
+            if (mapT.has(char)) {
+                mapT.set(char, mapT.get(char) + 1)
+            }
+            else (
+                mapT.set(char, 1)
+            )
+        }
+
+        return areMapsEqual(mapS,mapT)
+
+        function areMapsEqual(mapA,mapB){
+            if(mapA.size != mapB.size){return false;}
+
+            for(let [key,value] of mapA){
+                if(!mapB.has(key) || mapB.get(key) !== value){
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
